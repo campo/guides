a1d61e2463af6fa818a2f7e93682c90b8e9bccec
Joshua J. Campoverde <joshcampoverde@gmail.com>
2015-02-06 17:49:49 -0500

    Requesting access token and displaying user info on /me page

--- jawbone.rb
+++ jawbone.rb
@@ -1,16 +1,21 @@
+require 'rest-client'
 require 'sinatra/base'
 
 class MyApp < Sinatra::Base
   set :server, :thin
 
-  CLIENT_ID = "rgJ_zKbU5Zk"
-
+  CONFIG = YAML.load_file('.config')
+  CLIENT_ID = CONFIG[:client_id]
 
   get '/' do
     markdown :index
   end
 
   get '/oauthredirect' do
+    CONFIG['new_value'] = 'new'
+    CONFIG['code'] = params[:code]
+    request_access_token
+    File.open('.config', 'w+') {|f| f.write CONFIG.to_yaml }
     "Made it back here - code: #{params[:code]}"
   end
 
@@ -20,6 +25,24 @@ class MyApp < Sinatra::Base
 
   get '/about' do
     "About this app!"
+  end
+
+  get '/me' do
+    content_type :json
+    get_user_info.to_json
+  end
+
+  def request_access_token
+    token_url = "https://jawbone.com/auth/oauth2/token?client_id=#{CONFIG['client_id']}&client_secret=#{CONFIG['app_secret']}&grant_type=authorization_code&code=#{CONFIG['code']}"
+    response = JSON.parse(RestClient.get(token_url))
+    CONFIG['access_token'] = response["access_token"]
+  end
+
+  def get_user_info
+    request_url = "https://jawbone.com/nudge/api/v.1.1/users/@me"
+    request_headers = {:Authorization => "Bearer #{CONFIG['access_token']}" }
+    response = RestClient.get(request_url, request_headers)
+    response = JSON.parse(response)
   end
 
   run! if app_file == $0
